769a74465b0524bbaf87b32bad5f188e7b187dc9
Quinn Ramsay <qramsay@sfu.ca>
2018-03-30 23:49:42 -0600

    Initial Commit

---
+++ PersonalRecords/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PersonalRecords
+//
+//  Created by Quinn Ramsay on 2018-03-30.
+//  Copyright © 2018 Quinnter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
